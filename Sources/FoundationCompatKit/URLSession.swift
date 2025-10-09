82babb02c96629e1918d6e27a6353c4d7468b429
JWI <jwi@j-w-i.org>
2025-10-10 10:55:25 +1100

    adjustment

--- Sources/FoundationCompatKit/URLSession.swift
+++ Sources/FoundationCompatKit/URLSession.swift
@@ -4,18 +4,22 @@ import Foundation
 public class URLSession {
     public let configuration: URLSessionConfiguration
     public static let shared = URLSession(configuration: .default)
-
+    
     public init(configuration: URLSessionConfiguration) {
         self.configuration = configuration
     }
-
+    
+    public convenience init() {
+        self.init(configuration: .default)
+    }
+    
     public func dataTask(
         with request: URLRequest,
         completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
     ) -> URLSessionDataTask {
         return URLSessionDataTask(request: request, completionHandler: completionHandler)
     }
-
+    
     public func uploadTask(
         with request: URLRequest,
         from bodyData: Data?,
@@ -23,14 +27,14 @@ public class URLSession {
     ) -> URLSessionUploadTask {
         return URLSessionUploadTask(request: request, bodyData: bodyData, completionHandler: completionHandler)
     }
-
+    
     public func downloadTask(
         with request: URLRequest,
         completionHandler: @escaping (URL?, URLResponse?, Error?) -> Void
     ) -> URLSessionDownloadTask {
         return URLSessionDownloadTask(request: request, completionHandler: completionHandler)
     }
-
+    
     public func webSocketTask(with request: URLRequest) -> URLSessionWebSocketTask {
         return URLSessionWebSocketTask(url: request.url!)
     }
