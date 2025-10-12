08f8a67ce9f101ff4fce763183428bd63e182e95
JWI <jwi@j-w-i.org>
2025-10-12 23:06:01 +1100

    a

--- Sources/FoundationCompatKit/Timer.swift
+++ Sources/FoundationCompatKit/Timer.swift
@@ -1,46 +1,29 @@
-
-//
-//  Timer.swift
-//  Learning UIKit
-//
-//  Created by JWI on 28/08/2025.
-//  Copyright (c) 2025 JWI. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
-
-//iOS 10 Timer
+// iOS 10 Timer closure helper
 class ClosureTimer: NSObject {
     let block: (Foundation.Timer) -> Void
     
-    init(block: @escaping (Timer) -> Void) {
+    init(block: @escaping (Foundation.Timer) -> Void) {
         self.block = block
     }
     
-    @objc func fire(_ timer: Timer) {
+    @objc func fire(_ timer: Foundation.Timer) {
         block(timer)
     }
 }
 
-extension Timer {
+public extension Foundation.Timer {
     
-    ///iOS 10 Style Closure Based Timer
+    /// iOS 10-style closure-based timer for older iOS
     @available(iOS, introduced: 6.0, obsoleted: 10.0)
-    @nonobjc public class func scheduledTimer(withTimeInterval interval: TimeInterval, repeats: Bool, block: @escaping (Foundation.Timer) -> Void) -> Foundation.Timer {
+    static func scheduledTimer(withTimeInterval interval: TimeInterval, repeats: Bool, block: @escaping (Foundation.Timer) -> Void) -> Foundation.Timer {
         let closureTimer = ClosureTimer(block: block)
-        return Timer.scheduledTimer(timeInterval: interval,
-                                                      target: closureTimer,
-                                                      selector: #selector(ClosureTimer.fire(_:)),
-                                                      userInfo: nil,
-                                                      repeats: repeats)
+        return Foundation.Timer.scheduledTimer(timeInterval: interval,
+                                               target: closureTimer,
+                                               selector: #selector(ClosureTimer.fire(_:)),
+                                               userInfo: nil,
+                                               repeats: repeats)
     }
 }
-
-
-
-
-
-
-
