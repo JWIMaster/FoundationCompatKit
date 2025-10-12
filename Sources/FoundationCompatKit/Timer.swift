bd1026d6943ae938b75f90b4306046cf868698b1
JWI <jwi@j-w-i.org>
2025-10-12 23:10:20 +1100

    a

--- Sources/FoundationCompatKit/Timer.swift
+++ Sources/FoundationCompatKit/Timer.swift
@@ -1,29 +1,34 @@
 import Foundation
 import UIKit
 
-// iOS 10 Timer closure helper
-class ClosureTimer: NSObject {
-    let block: (Foundation.Timer) -> Void
-    
-    init(block: @escaping (Foundation.Timer) -> Void) {
-        self.block = block
-    }
-    
-    @objc func fire(_ timer: Foundation.Timer) {
-        block(timer)
-    }
-}
-
+#if !os(macOS) && !targetEnvironment(macCatalyst)
+@available(iOS, introduced: 6.0, obsoleted: 10.0)
 public extension Foundation.Timer {
-    
-    /// iOS 10-style closure-based timer for older iOS
+
+    /// Helper class for closure-based timer
+    class ClosureTimer: NSObject {
+        let block: (Foundation.Timer) -> Void
+
+        init(block: @escaping (Foundation.Timer) -> Void) {
+            self.block = block
+        }
+
+        @objc func fire(_ timer: Foundation.Timer) {
+            block(timer)
+        }
+    }
+
+    /// iOS 10-style closure-based scheduledTimer backport
     @available(iOS, introduced: 6.0, obsoleted: 10.0)
     static func scheduledTimer(withTimeInterval interval: TimeInterval, repeats: Bool, block: @escaping (Foundation.Timer) -> Void) -> Foundation.Timer {
         let closureTimer = ClosureTimer(block: block)
-        return Foundation.Timer.scheduledTimer(timeInterval: interval,
-                                               target: closureTimer,
-                                               selector: #selector(ClosureTimer.fire(_:)),
-                                               userInfo: nil,
-                                               repeats: repeats)
+        return Foundation.Timer.scheduledTimer(
+            timeInterval: interval,
+            target: closureTimer,
+            selector: #selector(ClosureTimer.fire(_:)),
+            userInfo: nil,
+            repeats: repeats
+        )
     }
 }
+#endif
