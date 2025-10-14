4934a727790e6170205c9060a7a6771030ee324f
JWI <jwi@j-w-i.org>
2025-10-14 21:21:03 +1100

    a

--- Sources/FoundationCompatKit/String.swift
+++ Sources/FoundationCompatKit/String.swift
@@ -3,18 +3,12 @@ import Foundation
 public extension String {
     /// Backport of addingPercentEncoding(withAllowedCharacters:) for iOS 6+
     func urlPathPercentEncoded() -> String {
-        if #available(iOS 7, *) {
-            // Modern API available
-            return self.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? self
-        } else {
-            // iOS 6 fallback
-            let allowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~/"
-            let allowedSet = NSMutableCharacterSet(charactersIn: allowed)
-            
-            // Use legacy NSStringEncoding
-            let encoding: String.Encoding = String.Encoding.utf8
-            
-            return (self as NSString).addingPercentEscapes(using: encoding.rawValue) ?? self
-        }
+        let allowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~/"
+        let allowedSet = NSMutableCharacterSet(charactersIn: allowed)
+        
+        // Use legacy NSStringEncoding
+        let encoding: String.Encoding = String.Encoding.utf8
+        
+        return (self as NSString).addingPercentEscapes(using: encoding.rawValue) ?? self
     }
 }
