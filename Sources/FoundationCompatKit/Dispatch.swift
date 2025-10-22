c79fe40a154ae095b34a0d538368114a88e68abe
JWI <jwi@j-w-i.org>
2025-10-22 15:19:18 +1100

    a

--- Sources/FoundationCompatKit/Dispatch.swift
+++ Sources/FoundationCompatKit/Dispatch.swift
@@ -7,6 +7,43 @@
 
 import Foundation
 
+@available(iOS, introduced: 6.0, deprecated: 8.0)
+public struct LegacyQoS {
+    public enum QoS {
+        case userInteractive
+        case userInitiated
+        case `default`
+        case utility
+        case background
+    }
+
+    public let qos: QoS
+
+    public init(_ qos: QoS) {
+        self.qos = qos
+    }
+
+    fileprivate var legacyPriority: DispatchQueue.GlobalQueuePriority {
+        switch qos { // <-- switch on the enum property, not self
+        case .userInteractive, .userInitiated:
+            return .high
+        case .`default`:
+            return .default
+        case .utility:
+            return .low
+        case .background:
+            return .background
+        }
+    }
+}
+
+public extension DispatchQueue {
+    class func global(qos: LegacyQoS.QoS) -> DispatchQueue {
+        return DispatchQueue.global(priority: LegacyQoS(qos).legacyPriority)
+    }
+}
+
+
 public struct DispatchWorkItem {
     let block: () -> Void
 
@@ -31,8 +68,17 @@ public extension DispatchQueue {
     func asyncAfter(deadline: DispatchTime, execute workItem: DispatchWorkItem) {
         let deltaNano = max(deadline.uptimeNanoseconds - DispatchTime.now().uptimeNanoseconds, 0)
         let deltaSeconds = Double(deltaNano) / 1_000_000_000
-        DispatchQueue.main.asyncAfter(deadline: .now() + deltaSeconds) {
+        self.asyncAfter(deadline: .now() + deltaSeconds) {
             workItem.perform()
         }
     }
 }
+
+extension DispatchWorkItem {
+    func notify(qos: LegacyQoS.QoS = .default, queue: DispatchQueue = .main, block: @escaping () -> Void) {
+        queue.asyncAfter(deadline: .now()) {
+            block()
+        }
+    }
+}
+
