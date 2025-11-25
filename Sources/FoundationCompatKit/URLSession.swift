64c38757ed7e06be3d6966cfee6272e995276d80
JWI <jwi@j-w-i.org>
2025-11-25 20:34:05 +1100

    added extra stuff for urlsesionconfig

--- Sources/FoundationCompatKit/URLSession.swift
+++ Sources/FoundationCompatKit/URLSession.swift
@@ -5,16 +5,17 @@ public class URLSessionCompat: NSObject {
     public let configuration: URLSessionConfigurationCompat
     public var delegate: URLSessionDelegateCompat?
     public let delegateQueue: OperationQueue
+    
     public static let shared = URLSessionCompat(configuration: .default, delegateQueue: OperationQueue())
     
-    // MARK: - Designated initializer
+    // Designated initializer
     public init(configuration: URLSessionConfigurationCompat, delegate: URLSessionDelegateCompat? = nil, delegateQueue: OperationQueue = .main) {
         self.configuration = configuration
         self.delegate = delegate
         self.delegateQueue = delegateQueue
     }
     
-    // Convenience initializers
+    // Convenience initialisers
     public convenience init(configuration: URLSessionConfigurationCompat) {
         self.init(configuration: configuration, delegate: nil, delegateQueue: .main)
     }
@@ -23,12 +24,28 @@ public class URLSessionCompat: NSObject {
         self.init(configuration: .default, delegateQueue: .main)
     }
     
-    // MARK: - Tasks
+    // Prepare request by merging configuration headers
+    internal func prepareRequest(_ request: URLRequest) -> URLRequest {
+        var modified = request
+        
+        if let extra = configuration.httpAdditionalHeaders {
+            var headers = modified.allHTTPHeaderFields ?? [:]
+            for (key, value) in extra {
+                headers[key] = value
+            }
+            modified.allHTTPHeaderFields = headers
+        }
+        
+        return modified
+    }
+    
+    // Tasks
     public func dataTask(
         with request: URLRequest,
         completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
     ) -> URLSessionDataTaskCompat {
-        return URLSessionDataTaskCompat(session: self, request: request, completionHandler: completionHandler)
+        let prepared = prepareRequest(request)
+        return URLSessionDataTaskCompat(session: self, request: prepared, completionHandler: completionHandler)
     }
     
     public func uploadTask(
@@ -36,17 +53,20 @@ public class URLSessionCompat: NSObject {
         from bodyData: Data?,
         completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
     ) -> URLSessionUploadTaskCompat {
-        return URLSessionUploadTaskCompat(session: self, request: request, bodyData: bodyData, completionHandler: completionHandler)
+        let prepared = prepareRequest(request)
+        return URLSessionUploadTaskCompat(session: self, request: prepared, bodyData: bodyData, completionHandler: completionHandler)
     }
     
     public func downloadTask(
         with request: URLRequest,
         completionHandler: @escaping (URL?, URLResponse?, Error?) -> Void
     ) -> URLSessionDownloadTaskCompat {
-        return URLSessionDownloadTaskCompat(session: self, request: request, completionHandler: completionHandler)
+        let prepared = prepareRequest(request)
+        return URLSessionDownloadTaskCompat(session: self, request: prepared, completionHandler: completionHandler)
     }
     
     public func webSocketTask(with request: URLRequest) -> URLSessionWebSocketTask {
-        return URLSessionWebSocketTask(session: self, url: request.url!)
+        let prepared = prepareRequest(request)
+        return URLSessionWebSocketTask(session: self, url: prepared.url!)
     }
 }
--- Sources/FoundationCompatKit/URLSessionConfiguration.swift
+++ Sources/FoundationCompatKit/URLSessionConfiguration.swift
@@ -3,7 +3,11 @@ import Foundation
 @available(iOS, introduced: 6.0, deprecated: 7.0.1)
 public class URLSessionConfigurationCompat {
     public var timeoutIntervalForRequest: TimeInterval = 60
-    public var timeoutIntervalForResource: TimeInterval = 7*24*60*60
+    public var timeoutIntervalForResource: TimeInterval = 7 * 24 * 60 * 60
+    
+    // Additional headers support
+    public var httpAdditionalHeaders: [String: String]? = nil
+    
     public static let `default` = URLSessionConfigurationCompat()
     public static let ephemeral = URLSessionConfigurationCompat()
     public static let background = URLSessionConfigurationCompat()
